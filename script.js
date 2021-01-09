c622e8fc11e21e0b6c86ccb94d0db6076dd4d171
Vadim <vadmonbtc@gmail.com>
2021-01-10 01:57:46 +0200

    firstcommit

---
+++ script.js
@@ -0,0 +1,73 @@
+
+
+
+
+console.log('arr' + " - object");
+
+let incr = 20,
+    decr = 20;
+
+// ++incr;
+// --decr;
+
+console.log(incr++, decr--);
+console.log(incr, decr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert('Vadmon lox');
+
+//const res = confirm('Ti tyt?');
+//console.log(res);
+
+//const answ = +prompt('Какой любимый мэмчик?', '322');
+//console.log(answ + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Ваше имя', '');
+// answers[1] = prompt('Ваше фамилия', '');
+// answers[2] = prompt('Ваш возраст', '');
+
+// //document.write(answers);
+// console.log(answers); 
+
+// const catgor = 'boys';
+// console.log(`https://htmlacademy.ru/${catgor}/5/view`);
+
+// const user = "Vadim";
+// alert(`Соси, ${user}`);
